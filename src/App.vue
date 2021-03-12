13f70075b54e7a6bd5b44a50af0f7c99b672e8a2
Julien Noyer <j.noyer@digitalworkshop.fr>
2021-03-12 13:15:36 +0100

    First commit(je sais)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <router-view :key="$route.fullPath"></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  export default {
+    name: 'App',
+    data(){
+      return{
+        user: undefined
+      }
+    },
+    created(){
+      this.$store.dispatch('checkUser')
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+      this.$store.subscribe((mutations,/*  state */) => {
+        // Check mutation*/
+        if( mutations.type === "USER" ){
+            // Update display
+            this.user = mutations.payload.data;
+            console.log(this.user)
+        }
+      })
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
